af25cbbe90ef669530ba8c4cd0fec900b9dca82a
51simple <a308515050@gmail.com>
2025-02-17 23:10:24 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["101.33.123.182"];
-pub const PUBLIC_RS_PUB_KEY: &str = "NhIPiB31CRxcUOpIlWBn8MF5P3ddxxIYGmQ8TVpMtcs=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["124.156.152.33"];
+pub const PUBLIC_RS_PUB_KEY: &str = "6Ehe8lxQbz5mNJ3eZSGqlrgGGiMiR0Sfly2WUt1QDts=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
