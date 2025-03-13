7c327c4a8c12d07e124ec4f4515ad2c82f030444
51simple <a308515050@gmail.com>
2025-03-13 21:56:32 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -99,8 +99,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rdk.vnddns.work"];
-pub const PUBLIC_RS_PUB_KEY: &str = "gcI3YfiUbbJVYm5NnFw+CDRHQlFWxlH+QmYzmkM+72U=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["129.226.134.174"];
+pub const PUBLIC_RS_PUB_KEY: &str = "XF3mLPu2HsP9KUX92OVMjvYvdeuPn5mXzbWauaKbsxA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
